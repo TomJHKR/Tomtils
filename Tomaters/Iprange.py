01a0d5a347965b2636726c8c0e6456c3401d1343
TomJHKR <thomasjhkr@gmail.com>
2024-10-17 18:58:31 +1300

    bug fixes

--- Tomaters/Iprange.py
+++ Tomaters/Iprange.py
@@ -3,12 +3,9 @@ import pandas as pd
 
 def iprange(s):
     ip_list = []
-    if len(s) == 1:
-        ip_list += [str(ip) for ip in ipaddress.IPv4Network(s[0])]
-    else:
-        for line in s:
-            ip_list += [str(ip) for ip in ipaddress.IPv4Network(line)]
+    for line in s:
+        ip_list += [str(ip) for ip in ipaddress.IPv4Network(line)]
     filename = input("What to save file as: ")
     df = pd.DataFrame(ip_list)
-    df.to_csv(f"{filename}.csv", index=False)
+    df.to_csv(f"{filename}.csv", index=False, header=None)
     return "IP Range file saved to " + filename + ".csv"
