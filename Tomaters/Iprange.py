2a3519e346645df1d6eb036d6278e494e66cd46f
TomJHKR <thomasjhkr@gmail.com>
2024-10-17 18:32:41 +1300

    fixed iprange and templated gitignore

--- Tomaters/Iprange.py
+++ Tomaters/Iprange.py
@@ -3,8 +3,11 @@ import pandas as pd
 
 def iprange(s):
     ip_list = []
-    for line in s:
-        ip_list += [str(line) for ip in ipaddress.IPv4Network(line)]
+    if len(s) == 1:
+        ip_list += [str(ip) for ip in ipaddress.IPv4Network(s[0])]
+    else:
+        for line in s:
+            ip_list += [str(ip) for ip in ipaddress.IPv4Network(line)]
     filename = input("What to save file as: ")
     df = pd.DataFrame(ip_list)
     df.to_csv(f"{filename}.csv", index=False)
